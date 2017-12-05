afb56c28ae76c96a36973aa866b0b668d0d58373
markcardamis <markncardamis@gmail.com>
2017-12-05 17:10:19 +1100

    second commit

--- FantasticView/FantasticView.swift
+++ FantasticView/FantasticView.swift
@@ -14,6 +14,7 @@ class FantasticView : UIView {
     let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
     var colorCounter = 0
 
+
     
     override init(frame: CGRect) {
         super.init(frame: frame)
